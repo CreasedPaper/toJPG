e5a7194bce325a6b8a761c5712f54083b143b69f
CreasedPaper <rockettesusan@gmail.com>
2022-02-08 14:21:20 -0500

    works

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from PIL import Image
 import os
 
+filename = input("name of image you want to convert(make sure to include the current file extension ex: .png)")
+image = Image.open(filename)
+imagesize = os.path.getsize(filename)
+rgb = image.convert("RGB")
+rgb.save(filename + ".jpg")
+
